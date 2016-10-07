7de462842a39f8b53d9a0b66b849df84c7acb111
laserlens <laserlens@gmail.com>
2016-10-07 10:54:02 -0500

    remove unessary code

--- routes/likes/adrian.js
+++ routes/likes/adrian.js
@@ -3,8 +3,8 @@ var express = require('express');
 //bring in the router from express library
 var router = express.Router();
 
-//var that holds likes starts at 1 becused triggered with like button
-var adrianLikes = '1';
+//var that holds likes 
+var adrianLikes = '0';
 
 //on get send var adrian likes and adds plus one then turns to string
 router.get('/',function (req, res) {
